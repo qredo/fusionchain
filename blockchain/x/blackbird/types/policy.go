794f35fb55d957e21d747b795e633616587c8657
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-09-05 11:00:29 +0200

    feat(blockchain/blackbird): introduce PolicyHandle for verifying any Policy

--- blockchain/x/blackbird/types/policy.go
+++ blockchain/x/blackbird/types/policy.go
@@ -1,8 +1,12 @@
 package types
 
 import (
+	fmt "fmt"
+
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/qredo/fusionchain/policy"
 	"github.com/qredo/fusionchain/repo"
+	"gitlab.qredo.com/edmund/blackbird/verifier/golang/simple"
 )
 
 var _ repo.Object = (*Policy)(nil)
@@ -13,3 +17,33 @@ func (a *Policy) SetId(id uint64) {
 	a.Id = id
 }
 
+// PolicyHandle is a convenience wrapper around a Policy object.
+//
+// The type of the Policy.Policy, which is a google.protobuf.Any, must implement
+// `policy.Policy` interface.
+type PolicyHandle struct {
+	policy *Policy
+	cdc    codec.BinaryCodec
+}
+
+func NewPolicyHandle(cdc codec.BinaryCodec, p *Policy) *PolicyHandle {
+	return &PolicyHandle{
+		policy: p,
+		cdc:    cdc,
+	}
+}
+
+func (h *PolicyHandle) Verify(approvers policy.ApproverSet) error {
+	var m policy.Policy
+	err := h.cdc.UnpackAny(h.policy.Policy, &m)
+	if err != nil {
+		return fmt.Errorf("unpacking Any: %w", err)
+	}
+	return m.Verify(approvers)
+}
+
+var _ (policy.Policy) = (*BlackbirdPolicy)(nil)
+
+func (p *BlackbirdPolicy) Verify(approvers policy.ApproverSet) error {
+	return simple.Verify(p.Data, nil, nil, nil, approvers)
+}
