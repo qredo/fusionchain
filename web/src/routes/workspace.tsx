0c00839746dc71093d82690a1324ccee18526237
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-09-19 17:18:55 +0200

    feat(web): revamp workspace page

--- web/src/routes/workspace.tsx
+++ web/src/routes/workspace.tsx
@@ -1,4 +1,5 @@
 import { protoInt64 } from "@bufbuild/protobuf";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { useLoaderData } from "react-router";
 import { Params } from "react-router-dom";
 import { useKeplrAddress } from "../keplr";
@@ -10,18 +11,13 @@ import KeyRequests from "../components/key_requests";
 import { workspaceByAddress } from "../client/identity";
 import { useQuery } from "@tanstack/react-query";
 import Address from "../components/address";
-import { MsgAddWorkspaceOwner, MsgRemoveWorkspaceOwner } from "../proto/fusionchain/identity/tx_pb";
-import { useState } from "react";
+import { MsgRemoveWorkspaceOwner } from "../proto/fusionchain/identity/tx_pb";
+import AddWorkspaceOwnerForm from "@/components/add_workspace_owner_form";
+import { Button } from "@/components/ui/button";
 
 async function requestNewKey(creator: string, workspaceAddr: string, keyringId: number) {
   await keplrBuildAndBroadcast([
     new MsgNewKeyRequest({ keyringId: protoInt64.parse(keyringId), creator, workspaceAddr, keyType: KeyType.ECDSA_SECP256K1 }),
-  ]);
-}
-
-async function addOwner(creator: string, workspaceAddr: string, newOwner: string) {
-  await keplrBuildAndBroadcast([
-    new MsgAddWorkspaceOwner({ creator, workspaceAddr, newOwner }),
   ]);
 }
 
@@ -35,53 +31,84 @@ function Workspace() {
   const addr = useKeplrAddress();
   const { workspaceAddr } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
   const wsQuery = useQuery(["workspace", workspaceAddr], () => workspaceByAddress(workspaceAddr));
-  const [newOwner, setNewOwner] = useState("");
   const keyringId = 0;
+  const ws = wsQuery.data?.workspace;
+
+  if (!ws) {
+    return (
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Workspace {workspaceAddr} not found</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div className="px-6 mt-10">
-      <div className="flex flex-row justify-between items-center">
+    <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="flex items-center justify-between space-y-2">
         <div>
-          <h1 className="font-bold text-lg">Workspace {workspaceAddr}</h1>
+          <h2 className="text-2xl font-bold tracking-tight">Workspace {workspaceAddr}</h2>
+          <p className="text-muted-foreground">
+            Created by <Address address={ws.creator} />.
+          </p>
         </div>
       </div>
 
-      <div className="mt-10 flex flex-col">
-        <h2 className="font-bold">Owners:</h2>
-        <ul className="list-disc list-inside">
-          {
-            wsQuery.data?.workspace?.owners.map((owner) => (
-              <li key={owner}>
-                <Address address={owner} />
-                <button className="hover:bg-blue-200 ml-2 px-2 py-1 rounded-lg text-xs" onClick={() => removeOwner(addr, workspaceAddr, owner)}>❌</button>
-              </li>
-            ))
-          }
-        </ul>
-        <div className="flex flex-row items-center mt-4 gap-2">
-          <input className="px-3 py-1 border border-slate-200 rounded-lg" type="text" placeholder="Add new owner" value={newOwner} onChange={(v) => setNewOwner(v.target.value)} />
-          <button className="bg-slate-200 hover:bg-blue-200 px-3 py-1 rounded-lg" onClick={async () => {
-            await addOwner(addr, workspaceAddr, newOwner);
-            setNewOwner("");
-          }}>
-            Add
-          </button>
-        </div>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Policies</CardTitle>
+          <CardDescription>Policies define who can operate on this workspace or use its keys to generate and sign transactions.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <span>TODO</span>
+          </div>
+        </CardContent>
+        <CardFooter>
+        </CardFooter>
+      </Card>
 
-      <div className="mt-10 flex flex-row justify-between items-center">
-        <div>
-          <h2 className="font-bold">Keys</h2>
-        </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Owners</CardTitle>
+          <CardDescription>With default policies, owners will be able to perform actions such as adding other owners or signing transactions.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <ul className="flex flex-col space-y-1">
+              {ws.owners.map((owner) => (
+                <li key={owner} className="list-disc list-inside group">
+                  <Address address={owner} />
+                  <Button variant="destructive" className="opacity-20 px-2 py-0.5 ml-2 h-auto w-auto group-hover:opacity-100" onClick={() => removeOwner(addr, ws.address, owner)}>
+                    X
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <AddWorkspaceOwnerForm addr={addr} workspaceAddr={workspaceAddr} />
+        </CardFooter>
+      </Card>
 
-        <button className="bg-slate-200 hover:bg-blue-200 px-4 py-2 rounded-lg" onClick={() => requestNewKey(addr, workspaceAddr, keyringId)}>
-          Request a new key
-        </button>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Keys</CardTitle>
+          <CardDescription>Keys are used to derive blockchain addresses and sign transactions.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button onClick={() => requestNewKey(addr, workspaceAddr, keyringId)}>
+            Request a new key
+          </Button>
 
-      <KeyRequests workspaceAddr={workspaceAddr} />
-
-      <Keys workspaceAddr={workspaceAddr} />
+          <KeyRequests workspaceAddr={workspaceAddr} />
+          <Keys workspaceAddr={workspaceAddr} />
+        </CardContent>
+      </Card>
     </div>
   );
 }
