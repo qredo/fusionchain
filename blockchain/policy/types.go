a03121cb3c9b034b82e5465bc49c92fee50a3beb
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-09-19 09:57:09 +0200

    feat(blockchain/policy): add new PolicyPayload struct

--- blockchain/policy/types.go
+++ blockchain/policy/types.go
@@ -1,5 +1,10 @@
 package policy
 
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+)
+
 type ApproverSet map[string]bool
 
 func BuildApproverSet(approvers []string) ApproverSet {
@@ -10,8 +15,28 @@ func BuildApproverSet(approvers []string) ApproverSet {
 	return approverSet
 }
 
+type PolicyPayload struct {
+	cdc codec.BinaryCodec
+	any *cdctypes.Any
+}
+
+func NewPolicyPayload(cdc codec.BinaryCodec, any *cdctypes.Any) PolicyPayload {
+	return PolicyPayload{
+		cdc: cdc,
+		any: any,
+	}
+}
+
+func EmptyPolicyPayload() PolicyPayload {
+	return NewPolicyPayload(nil, nil)
+}
+
+func (p PolicyPayload) Unpack(out any) error {
+	return p.cdc.UnpackAny(p.any, out)
+}
+
 type Policy interface {
 	// Verify tries to verify the current policy. The returned error is nil if
 	// the policy is valid.
-	Verify(approvers ApproverSet) error
+	Verify(approvers ApproverSet, payload PolicyPayload) error
 }
