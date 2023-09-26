d20b89d91eac70c67d03c8bce1fb087700e5cf61
Sasha Duke <sasha.duke@qredo.com>
2023-09-26 01:57:27 +0100

    disable evmos-specific tests

--- blockchain/x/revenue/v1/keeper/params_test.go
+++ blockchain/x/revenue/v1/keeper/params_test.go
@@ -1,14 +1,14 @@
 package keeper_test
 
-import "github.com/qredo/fusionchain/x/revenue/v1/types"
+// import "github.com/qredo/fusionchain/x/revenue/v1/types"
 
-func (suite *KeeperTestSuite) TestParams() {
-	params := suite.app.RevenueKeeper.GetParams(suite.ctx)
-	params.EnableRevenue = true
-	suite.Require().Equal(types.DefaultParams(), params)
-	params.EnableRevenue = false
-	err := suite.app.RevenueKeeper.SetParams(suite.ctx, params)
-	suite.Require().NoError(err)
-	newParams := suite.app.RevenueKeeper.GetParams(suite.ctx)
-	suite.Require().Equal(newParams, params)
-}
+// func (suite *KeeperTestSuite) TestParams() {
+// 	params := suite.app.RevenueKeeper.GetParams(suite.ctx)
+// 	params.EnableRevenue = true
+// 	suite.Require().Equal(types.DefaultParams(), params)
+// 	params.EnableRevenue = false
+// 	err := suite.app.RevenueKeeper.SetParams(suite.ctx, params)
+// 	suite.Require().NoError(err)
+// 	newParams := suite.app.RevenueKeeper.GetParams(suite.ctx)
+// 	suite.Require().Equal(newParams, params)
+// }
