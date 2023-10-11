069f4bab1609a97346fbc37b4b72955cd1f00228
lazar955 <lukic.lazar@yahoo.com>
2023-10-11 16:48:36 +0100

    adds new option for all on query keys

--- blockchain/x/treasury/keeper/query_keys.go
+++ blockchain/x/treasury/keeper/query_keys.go
@@ -21,57 +21,59 @@ func (k Keeper) Keys(goCtx context.Context, req *types.QueryKeysRequest) (*types
 
 	store := ctx.KVStore(k.storeKey)
 	keyStore := prefix.NewStore(store, types.KeyPrefix(types.KeyKey))
-	var keyResponse []*types.KeyResponse
-	var pageResp *query.PageResponse
-	wTypes := []types.WalletRequestType{types.WalletRequestType_WALLET_REQUEST_TYPE_QRDO, types.WalletRequestType_WALLET_REQUEST_TYPE_ETH_SEPOLIA, types.WalletRequestType_WALLET_REQUEST_TYPE_ETH}
 
-	if req.Type != types.WalletRequestType_WALLET_REQUEST_TYPE_ALL {
-		wTypes = []types.WalletRequestType{req.Type}
-	}
+	keys, page, err := query.GenericFilteredPaginate(k.cdc, keyStore, req.Pagination, func(key []byte, value *types.Key) (*types.KeyResponse, error) {
+		if value.WorkspaceAddr != "" && value.WorkspaceAddr != req.WorkspaceAddr {
+			return nil, nil
+		}
 
-	for _, wType := range wTypes {
-		keys, page, err := query.GenericFilteredPaginate(k.cdc, keyStore, req.Pagination, func(key []byte, value *types.Key) (*types.KeyResponse, error) {
-			if value.WorkspaceAddr != "" && value.WorkspaceAddr != req.WorkspaceAddr {
-				return nil, nil
+		response := &types.KeyResponse{
+			Key: value,
+		}
+
+		if req.Type != types.WalletRequestType_WALLET_REQUEST_TYPE_UNSPECIFIED {
+			var wTypes []types.WalletRequestType
+			if req.Type != types.WalletRequestType_WALLET_REQUEST_TYPE_ALL {
+				wTypes = []types.WalletRequestType{req.Type}
+			} else {
+				wTypes = []types.WalletRequestType{types.WalletRequestType_WALLET_REQUEST_TYPE_QRDO, types.WalletRequestType_WALLET_REQUEST_TYPE_ETH_SEPOLIA, types.WalletRequestType_WALLET_REQUEST_TYPE_ETH}
 			}
-
-			response := &types.KeyResponse{
-				Key: value,
-			}
-
-			if wType != types.WalletRequestType_WALLET_REQUEST_TYPE_UNSPECIFIED {
+			for _, wType := range wTypes {
 				var (
-					address string
-					err     error
+					address    string
+					err        error
+					walletType types.WalletType
 				)
 				switch wType {
 				case types.WalletRequestType_WALLET_REQUEST_TYPE_QRDO:
 					address, err = types.FusionChainAddress(value)
-					response.Type = types.WalletType_WALLET_TYPE_QRDO
+					walletType = types.WalletType_WALLET_TYPE_QRDO
 				case types.WalletRequestType_WALLET_REQUEST_TYPE_ETH_SEPOLIA:
 					address, err = types.EthereumAddress(value)
-					response.Type = types.WalletType_WALLET_TYPE_ETH_SEPOLIA
+					walletType = types.WalletType_WALLET_TYPE_ETH_SEPOLIA
 				case types.WalletRequestType_WALLET_REQUEST_TYPE_ETH:
 					address, err = types.EthereumAddress(value)
-					response.Type = types.WalletType_WALLET_TYPE_ETH
+					walletType = types.WalletType_WALLET_TYPE_ETH
 				}
 				if err != nil {
 					return nil, err
 				}
-				response.Address = address
+				response.Wallets = append(response.Wallets, &types.WalletKeyResponse{
+					Address: address,
+					Type:    walletType,
+				})
 			}
-
-			return response, nil
-		}, func() *types.Key { return &types.Key{} })
-
-		if err != nil {
-			return nil, status.Error(codes.Internal, err.Error())
 		}
-		keyResponse = append(keyResponse, keys...)
-		pageResp = page
+
+		return response, nil
+	}, func() *types.Key { return &types.Key{} })
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
 	}
+
 	return &types.QueryKeysResponse{
-		Keys:       keyResponse,
-		Pagination: pageResp,
+		Keys:       keys,
+		Pagination: page,
 	}, nil
 }
