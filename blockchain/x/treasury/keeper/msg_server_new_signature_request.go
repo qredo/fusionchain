b5664f0e2d4eeec81a3c2375583fc36229089f1a
ATMackay <alexandertmackay@hotmail.co.uk>
2023-12-22 16:45:56 +0000

    fix again

--- blockchain/x/treasury/keeper/msg_server_new_signature_request.go
+++ blockchain/x/treasury/keeper/msg_server_new_signature_request.go
@@ -34,7 +34,7 @@ func (k msgServer) NewSignatureRequest(goCtx context.Context, msg *types.MsgNewS
 		return nil, fmt.Errorf("keyring is nil or is inactive")
 	}
 
-	act, err := k.policyKeeper.AddAction(ctx, msg.Creator, msg, ws.SignPolicyId, msg.Btl, key.Type)
+	act, err := k.policyKeeper.AddAction(ctx, msg.Creator, msg, ws.SignPolicyId, msg.Btl)
 	if err != nil {
 		return nil, err
 	}
