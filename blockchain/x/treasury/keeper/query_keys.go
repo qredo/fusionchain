edb555f6eb7a43b0a2b685d4b176b2f8271cd970
martin.schaffner <martin.schaffner@qredo.com>
2023-12-22 13:54:11 +0100

    add multikey support to keys query

--- blockchain/x/treasury/keeper/query_keys.go
+++ blockchain/x/treasury/keeper/query_keys.go
@@ -35,42 +35,60 @@ func (k Keeper) Keys(goCtx context.Context, req *types.QueryKeysRequest) (*types
 			Key: value,
 		}
 
-		var wTypes []types.WalletType
-		if req.Type != types.WalletType_WALLET_TYPE_UNSPECIFIED {
-			wTypes = []types.WalletType{req.Type}
-		}
-		if req.Type != types.WalletType_WALLET_TYPE_SUI {
-			wTypes = []types.WalletType{types.WalletType_WALLET_TYPE_SUI}
-		} else {
-			wTypes = []types.WalletType{types.WalletType_WALLET_TYPE_FUSION, types.WalletType_WALLET_TYPE_ETH, types.WalletType_WALLET_TYPE_CELESTIA}
-		}
-		for _, wType := range wTypes {
-			var (
-				address    string
-				err        error
-				walletType types.WalletType
-			)
-			switch wType {
-			case types.WalletType_WALLET_TYPE_FUSION:
-				address, err = types.FusionChainAddress(value)
-				walletType = types.WalletType_WALLET_TYPE_FUSION
-			case types.WalletType_WALLET_TYPE_ETH:
-				address, err = types.EthereumAddress(value)
-				walletType = types.WalletType_WALLET_TYPE_ETH
-			case types.WalletType_WALLET_TYPE_CELESTIA:
-				address, err = types.CelestiaAddress(value)
-				walletType = types.WalletType_WALLET_TYPE_CELESTIA
-			case types.WalletType_WALLET_TYPE_SUI:
-				address, err = types.SuiAddress(value)
-				walletType = types.WalletType_WALLET_TYPE_SUI
+		// var wTypes []types.WalletType
+		wTypesECDSA := []types.WalletType{types.WalletType_WALLET_TYPE_FUSION, types.WalletType_WALLET_TYPE_ETH, types.WalletType_WALLET_TYPE_CELESTIA}
+		wTypesEdDSA := []types.WalletType{types.WalletType_WALLET_TYPE_SUI}
+
+		if value.Type == types.KeyType_KEY_TYPE_ECDSA_SECP256K1 {
+			for _, wType := range wTypesECDSA {
+				var (
+					address    string
+					err        error
+					walletType types.WalletType
+				)
+
+				switch wType {
+				case types.WalletType_WALLET_TYPE_FUSION:
+					address, err = types.FusionChainAddress(value)
+					walletType = types.WalletType_WALLET_TYPE_FUSION
+				case types.WalletType_WALLET_TYPE_ETH:
+					address, err = types.EthereumAddress(value)
+					walletType = types.WalletType_WALLET_TYPE_ETH
+				case types.WalletType_WALLET_TYPE_CELESTIA:
+					address, err = types.CelestiaAddress(value)
+					walletType = types.WalletType_WALLET_TYPE_CELESTIA
+				}
+				if err != nil {
+					return nil, err
+				}
+				response.Wallets = append(response.Wallets, &types.WalletKeyResponse{
+					Address: address,
+					Type:    walletType,
+				})
 			}
-			if err != nil {
-				return nil, err
+		}
+
+		if value.Type == types.KeyType_KEY_TYPE_EDDSA_ED25519 {
+			for _, wType := range wTypesEdDSA {
+				var (
+					address    string
+					err        error
+					walletType types.WalletType
+				)
+
+				switch wType {
+				case types.WalletType_WALLET_TYPE_SUI:
+					address, err = types.SuiAddress(value)
+					walletType = types.WalletType_WALLET_TYPE_SUI
+				}
+				if err != nil {
+					return nil, err
+				}
+				response.Wallets = append(response.Wallets, &types.WalletKeyResponse{
+					Address: address,
+					Type:    walletType,
+				})
 			}
-			response.Wallets = append(response.Wallets, &types.WalletKeyResponse{
-				Address: address,
-				Type:    walletType,
-			})
 		}
 
 		return response, nil
